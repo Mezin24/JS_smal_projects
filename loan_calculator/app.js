2d9a99f7000cd6f8ba845a06fd2baf54af897ff8
Pavel Mezencev <mezencev24@mail.ru>
2021-11-29 11:25:04 +0300

    finished loan calculator

--- loan_calculator/app.js
+++ loan_calculator/app.js
@@ -1,14 +1,56 @@
-const loanInput = document.querySelector('#laon_amount');
-const interestInput = document.querySelector('#interest');
-const yearsInput = document.querySelector('#years');
-const mounthlyOutput = document.querySelector('#monthly');
-const totalOutput = document.querySelector('#total');
-const interestOutput = document.querySelector('#interest');
-const btn = document.querySelector('button');
+document.querySelector('#loan-form').addEventListener('submit', function (e) {
+  document.getElementById('results').classList.remove('d-block');
+  document.getElementById('loading').classList.add('d-block');
 
-function calcDisplayOutput(e) {
+  setTimeout(calcResult, 2000);
+
   e.preventDefault();
+});
 
-  console.log(loanInput.value);
+function calcResult() {
+  const amountEl = document.getElementById('amount');
+  const interestEl = document.getElementById('interest');
+  const yearsEl = document.getElementById('years');
+  // const calcEl = document.getElementById('calc');
+  const monthlyEl = document.getElementById('monthly');
+  const totalEl = document.getElementById('total');
+  const totalInterestEl = document.getElementById('total-interest');
+
+  const principal = +amountEl.value;
+  const calculatedInterest = +interestEl.value / 100 / 12;
+  const calculatedPayment = +yearsEl.value * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayment);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyEl.value = monthly.toFixed(2);
+    totalEl.value = (monthly * calculatedPayment).toFixed(2);
+    totalInterestEl.value = (monthly * calculatedPayment - principal).toFixed(
+      2
+    );
+    document.getElementById('results').classList.add('d-block');
+    document.getElementById('loading').classList.remove('d-block');
+  } else {
+    showError('Please check your numbers');
+  }
 }
-btn.addEventListener('click', calcDisplayOutput);
+
+function showError(error) {
+  const card = document.querySelector('.card');
+  const html = `
+  <div class="alert alert-danger"">
+  ${error}
+</div>
+  `;
+  card.insertAdjacentHTML('afterbegin', html);
+
+  document.getElementById('results').classList.add('d-none');
+  document.getElementById('loading').classList.remove('d-block');
+
+  setTimeout(removeError, 2000);
+}
+
+function removeError() {
+  document.querySelector('.alert').remove();
+}
